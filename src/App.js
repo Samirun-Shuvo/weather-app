859afac8ffad01c6cd348113d645ec173706b96b
Samirun-Shuvo <samirunshuvo@gmail.com>
2023-12-11 10:14:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import hotbg from "./assets/hotbg.jpg";
+// import coldbg from "./assets/coldbg.jpg";
+import Discription from "./components/Description.js";
 
 function App() {
+  // api=https://api.openweathermap.org/data/2.5/weather?q=paris&appid=dfdb1dcc8650a84dffa9797d5d3ecb6b
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{ backgroundImage: `url(${hotbg})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Enter city..." />
+            <button>°F</button>
+          </div>
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt=""
+              />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>34 °C</h1>
+            </div>
+          </div>
+
+          <Discription></Discription>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Description.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Description = () => {
+    return (
+        <div className="section section__description">
+            description
+        </div>
+    );
+};
+
+export default Description;
